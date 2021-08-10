97501f368360ad8081eaf5ee2558bd4f7866d267
Ravi Teja <raviteja96763@gmail.com>
2021-08-10 09:58:54 +0530

    FirstComponent

---
+++ src/firstcomponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+
+function FirstComponent() {
+    return (
+        <div>
+            <React.Fragment>
+                <div>
+
+                </div>
+                   <h2>This is my first component</h2>
+            </React.Fragment>
+        </div>
+    )
+}
+export default FirstComponent
